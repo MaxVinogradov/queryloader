a2710f2dc128b0d6c1ff24b10de1da300989dd5c
vinogradov.max <vinogradov.max97@gmail.com>
2017-01-17 00:42:24 +0200

    Fast commit. Comments will be later

---
+++ app/src/main/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by maksim on 17.01.2017.
+ */
+public class Test {
+
+    public static final String q = "fdd";
+
+    public static void main(String[] args) {
+        System.out.println("Hello World" + q);
+    }
+
+}
